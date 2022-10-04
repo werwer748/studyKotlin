9c0c69779fb0b4ad1e522a83267afb452c994480
hugo <hugo.kjg.444@gmail.com>
2022-10-04 18:31:45 +0900

    찍먹중

--- Bts_lecture/app/src/main/java/com/hugo/bts_lecture/MainActivity.kt
+++ Bts_lecture/app/src/main/java/com/hugo/bts_lecture/MainActivity.kt
@@ -1,11 +1,81 @@
 package com.hugo.bts_lecture
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // 화면을 보여주는 코드
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1. 화면이 클릭을 감지해야함.
+        val image1 = findViewById<ImageView>(R.id.bts_image_1)
+        val image2 = findViewById<ImageView>(R.id.bts_image_2)
+        val image3 = findViewById<ImageView>(R.id.bts_image_3)
+        val image4 = findViewById<ImageView>(R.id.bts_image_4)
+        val image5 = findViewById<ImageView>(R.id.bts_image_5)
+        val image6 = findViewById<ImageView>(R.id.bts_image_6)
+        val image7 = findViewById<ImageView>(R.id.bts_image_7)
+
+        image1.setOnClickListener {
+            Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            // 2. 화면이 클릭되면, 다음 화면으로 넘어가서 사진 크게 보여줌!
+            var intent = Intent(this, Bts1Activity::class.java)
+            startActivity(intent)
+        }
+
+        image2.setOnClickListener {
+            Toast.makeText(this, "2번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            // 2. 화면이 클릭되면, 다음 화면으로 넘어가서 사진 크게 보여줌!
+            var intent = Intent(this, Bts2Activity::class.java)
+            startActivity(intent)
+        }
+
+        image3.setOnClickListener {
+            Toast.makeText(this, "3번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            // 2. 화면이 클릭되면, 다음 화면으로 넘어가서 사진 크게 보여줌!
+            var intent = Intent(this, Bts3Activity::class.java)
+            startActivity(intent)
+        }
+
+        image4.setOnClickListener {
+            Toast.makeText(this, "4번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            // 2. 화면이 클릭되면, 다음 화면으로 넘어가서 사진 크게 보여줌!
+            var intent = Intent(this, Bts4Activity::class.java)
+            startActivity(intent)
+        }
+
+        image5.setOnClickListener {
+            Toast.makeText(this, "5번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            // 2. 화면이 클릭되면, 다음 화면으로 넘어가서 사진 크게 보여줌!
+            var intent = Intent(this, Bts5Activity::class.java)
+            startActivity(intent)
+        }
+
+        image6.setOnClickListener {
+            Toast.makeText(this, "6번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            // 2. 화면이 클릭되면, 다음 화면으로 넘어가서 사진 크게 보여줌!
+            var intent = Intent(this, Bts6Activity::class.java)
+            startActivity(intent)
+        }
+
+        image7.setOnClickListener {
+            Toast.makeText(this, "7번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            // 2. 화면이 클릭되면, 다음 화면으로 넘어가서 사진 크게 보여줌!
+            var intent = Intent(this, Bts7Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
